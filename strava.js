598a7fb8c5d1b7c967dbff25d3c128af39795da8
florian <florian.carriere@gmail.com>
2021-05-22 21:51:09 +0200

    d

--- strava.js
+++ strava.js
@@ -1,4 +1,16 @@
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
+
+  $.ajax({
+          url: 'https://www.strava.com/api/v3/athlete',
+          beforeSend: function(xhr) {
+               xhr.setRequestHeader("Authorization", "Bearer 409b05e234c62cd9800860816109f3646aa82d73")
+          }, success: function(data){
+              alert(data);
+              //process the JSON data etc
+          }
+  })
+
+
+  //strava_response = curl -X GET https://www.strava.com/api/v3/athlete -H 'Authorization: Bearer 409b05e234c62cd9800860816109f3646aa82d73';
+  //document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
-// curl -X GET https://www.strava.com/api/v3/athlete -H 'Authorization: Bearer 409b05e234c62cd9800860816109f3646aa82d73'
