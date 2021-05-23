5f351bf0999e8a62441ed2a85edca0dd796ace9e
florian <florian.carriere@gmail.com>
2021-05-23 11:04:40 +0200

    test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myFunction() {
   let element = document.getElementById("demo");
   element.innerHTML = "Le texte modifié par le script.";
-  element.style.color = blue;
+  element.style.color = "blue";
 }
 //
   //strava_response = curl -X GET https://www.strava.com/api/v3/athlete -H 'Authorization: Bearer 409b05e234c62cd9800860816109f3646aa82d73';
