43724b14fa592a301ea369ba1b04e9a376ca3925
florian <florian.carriere@gmail.com>
2021-05-29 15:01:32 +0200

    add strava

---
+++ strava_api.js
@@ -0,0 +1,26 @@
+function getActivities(res){
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+    fetch(activities_link)
+        .then((res) => console.log(res.json()))
+}
+
+function reAuthorize(){
+  const auth_link = "https://www.strava.com/oauth/token"
+  fetch(auth_link,{
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      client_id: '60862',
+      client_secret: '2de20b9281f033da472a94eb775743eaa95bd49d',
+      refresh_token: '1fa2b3a8d0efb1df86c7f4ed1d67fc03e89ea3ac',
+      grant_type: 'refresh_token'
+    })
+  })
+    .then(res => res.json())
+    .then(res => getActivites(res))
+}
+
+reAuthorize()
