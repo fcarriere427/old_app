6dd49b742e4b2bd1d3f880b3b624c40b5c3c714d
florian <florian.carriere@gmail.com>
2021-05-29 15:56:27 +0200

    get strava actitivities list !

--- strava_api.js
+++ strava_api.js
@@ -1,9 +1,19 @@
 function getActivities(res){
-    // appelle API strava avec l'access token qu'on vient de renouveller
-    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
-    fetch(activities_link)
-    // affiche le contenu
-      .then(res => document.getElementById("main").innerHTML = res.json())  
+  // appelle API strava avec l'access token qu'on vient de renouveller
+  const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+  fetch(activities_link)
+    .then(function (response) {return response.json();})
+    .then(function (data) {appendData(data);})
+    .catch(function (err) {console.log('error: ' + err);})
+}
+
+function appendData(data) {
+  var mainContainer = document.getElementById("main");
+  for (var i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = 'Name: ' + data[i].name + '  //  Distance: ' + data[i].distance;
+    mainContainer.appendChild(div);
+  }
 }
 
 function reAuthorize(){
