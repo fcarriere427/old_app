5887b60f239bba7a844125df01a9fc4d8e0e964a
florian <florian.carriere@gmail.com>
2022-01-23 17:18:20 +0100

    correct speed

--- strava_api.js
+++ strava_api.js
@@ -15,7 +15,7 @@ function appendData(data) {
       + '  // Date: ' + data[i].start_date
       + '  // Distance: ' + Math.round(data[i].distance / 1000 * 100) / 100 + 'km'
       + '  // Time: ' + Math.round(data[i].moving_time/60 * 100) / 100 + "mn"
-      + '  // Avg speed: ' + Math.round(data[i].average_speed * 0.06 * 100) / 100 + "mn/km";
+      + '  // Avg speed: ' + Math.round(1000 / 60 / data[i].average_speed * 100) / 100 + "mn/km";
     mainContainer.appendChild(div);
   }
 }
