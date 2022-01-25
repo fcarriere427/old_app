ad3321e7f4657107c188d3c104f34acff1eb266f
florian <florian.carriere@gmail.com>
2022-01-25 22:50:39 +0100

    test

--- strava_api.js
+++ strava_api.js
@@ -11,7 +11,7 @@ function appendData(data) {
   var mainContainer = document.getElementById("main");
   // test
   var test = document.createElement("div");
-  test.innerHTML = 'data = ' + data;
+  test.innerHTML = 'data[1].ID = ' + data[1].ID;
   mainContainer.appendChild(test);
 
   for (var i = 0; i < data.length; i++) {
