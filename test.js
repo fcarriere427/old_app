287dfc2894f81feed30c8ecfc7160a06b5cc816e
florian <florian.carriere@gmail.com>
2022-01-29 05:59:35 +0100

    Create test page for FE/BE tests

---
+++ test.js
@@ -0,0 +1,33 @@
+function displayData() {
+  var mainContainer = document.getElementById("main");
+
+// ******* REPRENDRE ICI ************* //
+// On doit appeler le server, puis afficher les données //
+  fetch("http://localhost:3000/activities")
+      // .then((response) => response.json())
+      // .then((responseJson) => {
+      //   setPlayers(responseJson.data);
+      var div = document.createElement("div");
+      div.innerHTML = 'on a bien appelé la route "activities"';
+      mainContainer.appendChild(div);
+  });
+
+// // test
+//   var test = document.createElement("div");
+//   test.innerHTML = 'data[0].id = ' + data[0].id;
+//   mainContainer.appendChild(test);
+}
+
+// function readTextFile(file, callback) {
+//       var rawFile = new XMLHttpRequest();
+//     rawFile.overrideMimeType("application/json");
+//     rawFile.open("GET", file, true);
+//     rawFile.onreadystatechange = function() {
+//         if (rawFile.readyState === 4 && rawFile.status == "200") {
+//             callback(rawFile.responseText);
+//         }
+//     }
+//     rawFile.send(null);
+// }
+
+displayData()
