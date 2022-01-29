cf45239dfe679ace8474932b695795726e5e2eb5
florian <florian.carriere@gmail.com>
2022-01-29 07:26:47 +0100

    back to localhost

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ function displayData() {
   var div = document.createElement("div");
   div.innerHTML = 'on va afficher qq chose';
   mainContainer.appendChild(div);
-  fetch("test")
+  fetch("http://localhost:3000")
     .then(response=>response.json())
     .then(json => {
       console.log(json);
