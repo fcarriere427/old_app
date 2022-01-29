bc0fd3debadddb8219962ba67e43fb634e773060
florian <florian.carriere@gmail.com>
2022-01-29 08:43:25 +0100

    test

--- test.js
+++ test.js
@@ -6,15 +6,16 @@ function displayData() {
   div.innerHTML = 'on va afficher qq chose';
   mainContainer.appendChild(div);
   fetch("/activities")
-    .then(response=>response.json())
-    .then(json => {
-      // ******* REPRENDRE ICI ************* //
-      // que récupère-t-on de la route activities ??? //
-      //console.log(json);
-      var div = document.createElement("div");
-      div.innerHTML = 'on a bien appelé la route "activities", qui répond : ' + json;
-      mainContainer.appendChild(div);
-    })
+    .then(response => response.json())
+    .then(json => console.log(json)
+    // .then(json => {
+    //   // ******* REPRENDRE ICI ************* //
+    //   // que récupère-t-on de la route activities ??? //
+    //   var div = document.createElement("div");
+    //   div.innerHTML = 'on a bien appelé la route "activities", qui répond : ' + json;
+    //   mainContainer.appendChild(div);
+    // })
+    //
     .catch((err) => {
       console.log('Fetch problem: ' + err.message);
     });
