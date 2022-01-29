f7e09998186ddf3a89a1baf07b0b5673cf77a4ec
florian <florian.carriere@gmail.com>
2022-01-29 09:56:44 +0100

    change response

--- test.js
+++ test.js
@@ -6,16 +6,17 @@ function displayData() {
   div.innerHTML = 'on va afficher qq chose';
   mainContainer.appendChild(div);
   fetch("/activities")
-    .then(response => response.json())
-    .then(json => {
-      console.log("response.json = " + json.data);
-      // ******* REPRENDRE ICI ************* //
-      // intégrer la boucle qui affiche toutes les données strava
-      var div = document.createElement("div");
-      div.innerHTML = 'on a bien appelé la route "activities", qui répond : ' + json.data;
-      mainContainer.appendChild(div);
-    })
-    //
+    .then(response => console.log(response))
+
+    // .then(response => response.json())
+    // .then(json => {
+    //   console.log("response.json = " + json.data);
+    //   // ******* REPRENDRE ICI ************* //
+    //   // intégrer la boucle qui affiche toutes les données strava
+    //   var div = document.createElement("div");
+    //   div.innerHTML = 'on a bien appelé la route "activities", qui répond : ' + json.data;
+    //   mainContainer.appendChild(div);
+    // })
     .catch((err) => {
       console.log('Fetch problem: ' + err.message);
     });
