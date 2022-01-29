2c99f474db44f072991a1a8745136e35321b2b0c
florian <florian.carriere@gmail.com>
2022-01-29 06:01:55 +0100

    update

--- test.js
+++ test.js
@@ -3,20 +3,23 @@ function displayData() {
 
 // ******* REPRENDRE ICI ************* //
 // On doit appeler le server, puis afficher les données //
-  fetch("http://localhost:3000/activities")
-      // .then((response) => response.json())
-      // .then((responseJson) => {
-      //   setPlayers(responseJson.data);
-      var div = document.createElement("div");
-      div.innerHTML = 'on a bien appelé la route "activities"';
-      mainContainer.appendChild(div);
-  });
+    var div = document.createElement("div");
+    div.innerHTML = 'on va afficher qq chose"';
+    mainContainer.appendChild(div);
+    fetch("http://localhost:3000/activities")
+          // .then((response) => response.json())
+          // .then((responseJson) => {
+          //   setPlayers(responseJson.data);
+          var div = document.createElement("div");
+          div.innerHTML = 'on a bien appelé la route "activities"';
+          mainContainer.appendChild(div);
+    });
 
 // // test
 //   var test = document.createElement("div");
 //   test.innerHTML = 'data[0].id = ' + data[0].id;
 //   mainContainer.appendChild(test);
-}
+  }
 
 // function readTextFile(file, callback) {
 //       var rawFile = new XMLHttpRequest();
