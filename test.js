0663a71d53314fd6bc71f769560a71c3f562496c
florian <florian.carriere@gmail.com>
2022-01-30 12:02:57 +0100

    Display all activities

--- test.js
+++ test.js
@@ -18,9 +18,21 @@ function displayData() {
 
        var mainContainer = document.getElementById("main");
        // test
-       var test = document.createElement("div");
-       test.innerHTML = 'data[0].id = ' + data[0].id;
-       mainContainer.appendChild(test);
+       // var test = document.createElement("div");
+       // test.innerHTML = 'data[0].id = ' + data[0].id;
+       // mainContainer.appendChild(test);
+
+       for (var i = 0; i < data.length; i++) {
+         var div = document.createElement("div");
+         // pour référence : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
+         div.innerHTML = 'ID: ' + data[i].id
+           + '  // Date: ' + data[i].start_date
+           + '  // Distance: ' + Math.round(data[i].distance / 1000 * 100) / 100 + 'km'
+           + '  // Time: ' + Math.round(data[i].moving_time/60 * 100) / 100 + "mn"
+           + '  // Avg speed: ' + Math.round(1000 / 60 / data[i].average_speed * 100) / 100 + "mn/km";
+         mainContainer.appendChild(div);
+       }
+
     })
     .catch((err) => {
       console.log('Fetch problem: ' + err.message);
