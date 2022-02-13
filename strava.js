a1581525e056927facac6b54158506b66ca7e631
florian <florian.carriere@gmail.com>
2022-02-13 08:48:45 +0100

    display one activity

--- strava.js
+++ strava.js
@@ -1,18 +1,28 @@
+// pour référence : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
+
+
 function displayData() {
   fetch("/activities")
     .then(response => response.json())
     .then(data => {
+      // *** pour afficher la liste des activités
         var mainContainer = document.getElementById("main");
-        for (var i = 0; i < data.length; i++) {
         var div = document.createElement("div");
-         // pour référence : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
-        div.innerHTML = 'ID: ' + data[i].id
-           + '  // Date: ' + data[i].start_date
-           + '  // Distance: ' + Math.round(data[i].distance / 1000 * 100) / 100 + 'km'
-           + '  // Time: ' + Math.round(data[i].moving_time/60 * 100) / 100 + "mn"
-           + '  // Avg speed: ' + Math.round(1000 / 60 / data[i].average_speed * 100) / 100 + "mn/km";
+        div.innerHTML = 'data: ' + data.json();
         mainContainer.appendChild(div);
       }
+    // .then(data => {
+      // *** pour afficher la liste des activités
+        // var mainContainer = document.getElementById("main");
+        // for (var i = 0; i < data.length; i++) {
+        // var div = document.createElement("div");
+        // div.innerHTML = 'ID: ' + data[i].id
+        //    + '  // Date: ' + data[i].start_date
+        //    + '  // Distance: ' + Math.round(data[i].distance / 1000 * 100) / 100 + 'km'
+        //    + '  // Time: ' + Math.round(data[i].moving_time/60 * 100) / 100 + "mn"
+        //    + '  // Avg speed: ' + Math.round(1000 / 60 / data[i].average_speed * 100) / 100 + "mn/km";
+      //   mainContainer.appendChild(div);
+      // }
     })
     .catch((err) => {
       console.log('"Activities" fetch problem: ' + err.message);
