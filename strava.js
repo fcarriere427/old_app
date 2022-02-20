3919bb9f11a590416545fec099db4673b8624ed4
florian <florian.carriere@gmail.com>
2022-02-20 13:56:32 +0100

    typo

--- strava.js
+++ strava.js
@@ -16,13 +16,14 @@ function displayData() {
         // pour afficher la liste des activités
         var mainContainer = document.getElementById("main");
         for (var i = 0; i < data.length; i++) {
-        var div = document.createElement("div");
-        div.innerHTML = 'ID: ' + data[i].id
+          var div = document.createElement("div");
+          div.innerHTML = 'ID: ' + data[i].id
            + '  // Date: ' + data[i].start_date
            + '  // Distance: ' + Math.round(data[i].distance / 1000 * 100) / 100 + 'km'
            + '  // Time: ' + Math.round(data[i].moving_time/60 * 100) / 100 + "mn"
            + '  // Avg speed: ' + Math.round(1000 / 60 / data[i].average_speed * 100) / 100 + "mn/km";
-        mainContainer.appendChild(div);
+          mainContainer.appendChild(div);
+        }
       })
     .catch((err) => {
       console.log('"Activities" fetch problem: ' + err.message);
