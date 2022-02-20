8c212f1d7628c105daae691f81d1c4747a1c113a
florian <florian.carriere@gmail.com>
2022-02-20 14:49:31 +0100

    update

--- strava.js
+++ strava.js
@@ -20,17 +20,17 @@ function displayData() {
           // calculs sur le temps
           moving_time = data[i].moving_time;
           mn_moving_time = Math.round(data[i].moving_time/60);
-          sec_moving_time = Math.round((moving_time - mn_moving_time) * 100) / 100 ;
+          sec_moving_time = Math.round(moving_time - mn_moving_time*60);
           // calculs sur la vitesse
           avg_speed = data[i].average_speed;
-          mn_avg_speed = Math.round(1000 / 60 / data[i].average_speed * 100) / 100
-          sec_avg_speed = Math.round((avg_speed - mn_avg_speed) * 100) / 100 ;
+          mn_avg_speed = Math.round(1 / (60 * avg_speed / 1000));
+          sec_avg_speed = Math.round(((1/(60*avg_speed /1000)) - Y) * 60);
 
-          div.innerHTML = 'ID: ' + data[i].id
-           + ' - Date: ' + data[i].start_date
-           + ' - Distance: ' + Math.round(data[i].distance / 1000 * 100) / 100 + 'km'
-           + ' - Time: ' + mn_moving_time + "mn" + sec_moving_time + "s"
-           + ' - Avg speed: ' + mn_avg_speed + ' mn ' + sec_avg_speed + ' sec/km';
+          div.innerHTML = '[' + data[i].id + ']'
+           + ' - ' + data[i].start_date
+           + ' - ' + Math.round(data[i].distance / 1000 * 100) / 100 + 'km'
+           + ' - ' + mn_moving_time + "mn" + sec_moving_time + "s"
+           + ' - ' + mn_avg_speed + ' mn ' + sec_avg_speed + ' sec/km';
           mainContainer.appendChild(div);
         }
       })
