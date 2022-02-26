bcb0a1805781c65830f57f85e457c2333be2d9bc
florian <florian.carriere@gmail.com>
2022-02-26 01:12:52 +0100

    test

--- strava.js
+++ strava.js
@@ -28,29 +28,28 @@ function listActivities() {
     var mainContainer = document.getElementById('main');
     for (var i = 0; i < data.length; i++) {
       var div = document.createElement("div");
-      // // calculs sur le temps
-      // // pour référence : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
-      // moving_time = data[i].moving_time; // en secondes
-      // h_moving_time = Math.round(moving_time/3600);
-      // mn_moving_time = Math.round((moving_time - h_moving_time * 3600) / 60);
-      // sec_moving_time = Math.round(moving_time - h_moving_time * 3600 - mn_moving_time * 60);
-      // if(h_moving_time > 0){
-      //   time_str = h_moving_time + 'h' + mn_moving_time + 'mn' + sec_moving_time + 's';
-      // } else {
-      //   time_str = mn_moving_time + 'mn' + sec_moving_time + 's';
-      // }
-      // // calculs sur la vitesse
-      // avg_speed = data[i].average_speed; // en mètres/secondes
-      // mn_avg_speed = Math.round(1 / (60 * avg_speed / 1000));
-      // sec_avg_speed = Math.round(((1/(60*avg_speed /1000)) - mn_avg_speed) * 60);
-      // // concaténation de la chaine pour 1 activité
-      // var str = '[' + data[i].id + '] '
-      //  + data[i].start_date.substring(0,10)
-      //  + ' - ' + Math.round(data[i].distance / 1000 * 100) / 100 + 'km'
-      //  + ' - ' + time_str
-      //  + ' - ' + mn_avg_speed + 'mn' + sec_avg_speed + 'sec/km'
-      // // injection dans la page
-      var str = data[i].doc.start_date;
+      // calculs sur le temps
+      // pour référence : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
+      moving_time = data[i].doc.moving_time; // en secondes
+      h_moving_time = Math.round(moving_time/3600);
+      mn_moving_time = Math.round((moving_time - h_moving_time * 3600) / 60);
+      sec_moving_time = Math.round(moving_time - h_moving_time * 3600 - mn_moving_time * 60);
+      if(h_moving_time > 0){
+        time_str = h_moving_time + 'h' + mn_moving_time + 'mn' + sec_moving_time + 's';
+      } else {
+        time_str = mn_moving_time + 'mn' + sec_moving_time + 's';
+      }
+      // calculs sur la vitesse
+      avg_speed = data[i].doc.average_speed; // en mètres/secondes
+      mn_avg_speed = Math.round(1 / (60 * avg_speed / 1000));
+      sec_avg_speed = Math.round(((1/(60*avg_speed /1000)) - mn_avg_speed) * 60);
+      // concaténation de la chaine pour 1 activité
+      var str = '[' + data[i].doc.id + '] '
+       + data[i].doc.start_date.substring(0,10)
+       + ' - ' + Math.round(data[i].doc.distance / 1000 * 100) / 100 + 'km'
+       + ' - ' + time_str
+       + ' - ' + mn_avg_speed + 'mn' + sec_avg_speed + 'sec/km'
+      // injection dans la page
       div.innerHTML = str;
       mainContainer.appendChild(div);
     }
