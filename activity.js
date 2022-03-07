0addf0d7756f501af2e5f958a7820dfadc16b333
florian <florian.carriere@gmail.com>
2022-03-07 23:35:49 +0100

    test

--- activity.js
+++ activity.js
@@ -8,8 +8,7 @@ const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
 messageDiv.innerHTML = 'id de la requete = ' + id ;
 
-var encodedRoutes = ["{zkrIm`inANPD?BDXGPKLATHNRBRFtAR~AFjAHl@D|ALtATj@HHJBL?`@EZ?NQ\\Y^MZURGJKR]RMXYh@QdAWf@[~@aAFGb@?j@YJKBU@m@FKZ[NSPKTCRJD?`@Wf@Wb@g@HCp@Qh@]z@SRMRE^EHJZnDHbBGPHb@NfBTxBN|DVbCBdA^lBFl@Lz@HbBDl@Lr@Bb@ApCAp@Ez@g@bEMl@g@`B_AvAq@l@    QF]Rs@Nq@CmAVKCK?_@Nw@h@UJIHOZa@xA]~@UfASn@U`@_@~@[d@Sn@s@rAs@dAGN?NVhAB\\Ox@@b@S|A?Tl@jBZpAt@vBJhATfGJn@b@fARp@H^Hx@ARGNSTIFWHe@AGBOTAP@^\\zBMpACjEWlEIrCKl@i@nAk@}@}@yBOWSg@kAgBUk@Mu@[mC?QLIEUAuAS_E?uCKyCA{BH{DDgF`AaEr@uAb@oA~@{AE}AKw@    g@qAU[_@w@[gAYm@]qAEa@FOXg@JGJ@j@o@bAy@NW?Qe@oCCc@SaBEOIIEQGaAe@kC_@{De@cE?KD[H[P]NcAJ_@DGd@Gh@UHI@Ua@}Bg@yBa@uDSo@i@UIICQUkCi@sCKe@]aAa@oBG{@G[CMOIKMQe@IIM@KB]Tg@Nw@^QL]NMPMn@@\\Lb@P~@XT",
-"u}krIq_inA_@y@My@Yu@OqAUsA]mAQc@CS@o@FSHSp@e@n@Wl@]ZCFEBK?OC_@Qw@?m@CSK[]]EMBeAA_@m@qEAg@UoCAaAMs@IkBMoACq@SwAGOYa@IYIyA_@kEMkC]{DEaAScC@yEHkGA_ALsCBiA@mCD{CCuAZcANOH@HDZl@Z`@RFh@\\TDT@ZVJBPMVGLM\\Mz@c@NCPMXERO|@a@^Ut@s@p@KJAJ    Bd@EHEXi@f@a@\\g@b@[HUD_B@uADg@DQLCLD~@l@`@J^TF?JANQ\\UbAyABEZIFG`@o@RAJEl@_@ZENDDIA[Ki@BURQZaARODKVs@LSdAiAz@G`BU^A^GT@PRp@zARXRn@`BlDHt@ZlAFh@^`BX|@HHHEf@i@FAHHp@bBd@v@DRAVMl@i@v@SROXm@tBILOTOLs@NON_@t@KX]h@Un@k@\\c@h@Ud@]ZGNKp@Sj@KJo@    b@W`@UPOX]XWd@UF]b@WPOAIBSf@QVi@j@_@V[b@Uj@YtAEFCCELARBn@`@lBjAzD^vB^hB?LENURkAv@[Ze@Xg@Py@p@QHONMA[HGAWE_@Em@Hg@AMCG@QHq@Cm@M[Jy@?UJIA{@Ae@KI@GFKNIX[QGAcAT[JK?OVMFK@IAIUKAYJI?QKUCGFIZCXDtAHl@@p@LjBCZS^ERAn@Fj@Br@Hn@HzAHh@RfD?j@TnCTlA    NjANb@\\z@TtARr@P`AFnAGfBG`@CFE?"]
+var encodedRoutes = ["{kiiHwguL`@_@Ze@Xk@Ts@Xm@tAiEPq@Fq@TcAJoBLu@Zq@Hc@t@wAVu@H}@BeAN_AM{@Fw@Lu@`@kBn@yAZe@Nu@Fw@AaAJsBz@sFOg@@s@wAuBsAf@kAYi@?c@Re@@c@Ty@c@q@Lc@Qg@g@[u@{AiC]cAQ{@Us@[g@g@Gc@[a@_@e@MsAZkAd@i@f@WfBSn@u@E@kLb@s@Zy@j@cHZ}CRqFv@cLRmE\\oDXgFXgDB_BLwBAw@BWP[Ew@P{B@eBr@_HPsBTeF@_APqBFuBHy@LwBRq@VkB?oBZk@d@JTp@Fr@@z@HpAJv@Dt@Jt@Zj@d@TPn@Tj@t@vCZj@Tp@lBjAnA`ArAn@dAr@b@TjANvBFlAXf@RX[`@Wf@Kd@@d@T^b@Vl@Nv@BdAxEdGVb@XZbAp@~CzFf@t@bCjEpBdEv@nAlBjDPn@D`@DdCLp@Rr@l@vAZh@^b@dAl@nA^v@\\~B~BbAp@jAXtCMf@DhAb@dAn@f@l@tB|C~@~@~FdCxCrBb@\\Jp@Dv@GtBFz@Rp@v@pA`@^bAn@rBHfATnCnAb@X`A~@nDpF~@dA`@Td@Dd@E~Am@vBAb@P\\d@Rt@bA`Gd@RFHVt@tAbBb@VlBIpBHPVRr@Fh@v@dCC`AhA~@FL@j@CjBHz@C~@Iz@Sr@?NLnBm@bBIfBDjEDbBFz@Tn@Ft@W|@Kv@?x@Up@_A|@y@lAgAjC[h@_@d@s@~A_B`BmBZ_@J?FJz@`@hBJx@LbBLx@Fv@?dCIrDa@hFUrBcAfGiAxEq@|A]f@cCbC_BrA}@d@uDpAoBT_DLwF_@iAQ_D{@[g@a@OmBe@}EaBk@[}EmBcAk@a@a@oBeDq@aB_AuCW{AUoB@_AE{@?wBFu@Hu@j@}AXi@\\g@hBkBpAcBFa@s@sAaBwAmCsBQEi@d@e@Hy@@c@Uu@uA_AcAiCkBa@Ie@F_@d@c@Nc@?eAg@oAY_@`@_@h@iCnF_@^e@F_@U]e@gAcBuBsCw@qA_@c@[k@uBcDq@qAmBgDaFsG[g@_@a@yASeFiEy@iAQC]PWn@Op@gCdHqAbCgDdFMXAX}AvAa@f@Sr@GH"];
 
 fetch(`/strava_app/activity?id=${id}`)
 .then(response => response.json())
@@ -17,8 +16,8 @@ fetch(`/strava_app/activity?id=${id}`)
   console.log('Ici on a récupéré l\'activité normalement, par ex data.distance = ' + data.distance)
   //****** var encodedRoutes = data.polylines;
   // ici = remplacer encodedRoutes par un extract du champ d'un run, en traitant bien les backlash avant
-  console.log('encodedRoutes = ' + data.map.summary_polyline);
-  encodedRoutes = data.map.summary_polyline;
+  //console.log('encodedRoutes = ' + data.map.summary_polyline);
+  //encodedRoutes = data.map.summary_polyline;
   // Ajout de la  map
   var map = L.map('map').setView([55.609818, 13.003286], 13);
   L.tileLayer(
