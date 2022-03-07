21742ea3f553945ee0c8d937ec8d39a8493358d4
florian <florian.carriere@gmail.com>
2022-03-07 23:38:45 +0100

    test

--- activity.js
+++ activity.js
@@ -8,18 +8,15 @@ const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
 messageDiv.innerHTML = 'id de la requete = ' + id ;
 
-var encodedRoutes = ["{kiiHwguL`@_@Ze@Xk@Ts@Xm@tAiEPq@Fq@TcAJoBLu@Zq@Hc@t@wAVu@H}@BeAN_AM{@Fw@Lu@`@kBn@yAZe@Nu@Fw@AaAJsBz@sFOg@@s@wAuBsAf@kAYi@?c@Re@@c@Ty@c@q@Lc@Qg@g@[u@{AiC]cAQ{@Us@[g@g@Gc@[a@_@e@MsAZkAd@i@f@WfBSn@u@E@kLb@s@Zy@j@cHZ}CRqFv@cLRmE\\oDXgFXgDB_BLwBAw@BWP[Ew@P{B@eBr@_HPsBTeF@_APqBFuBHy@LwBRq@VkB?oBZk@d@JTp@Fr@@z@HpAJv@Dt@Jt@Zj@d@TPn@Tj@t@vCZj@Tp@lBjAnA`ArAn@dAr@b@TjANvBFlAXf@RX[`@Wf@Kd@@d@T^b@Vl@Nv@BdAxEdGVb@XZbAp@~CzFf@t@bCjEpBdEv@nAlBjDPn@D`@DdCLp@Rr@l@vAZh@^b@dAl@nA^v@\\~B~BbAp@jAXtCMf@DhAb@dAn@f@l@tB|C~@~@~FdCxCrBb@\\Jp@Dv@GtBFz@Rp@v@pA`@^bAn@rBHfATnCnAb@X`A~@nDpF~@dA`@Td@Dd@E~Am@vBAb@P\\d@Rt@bA`Gd@RFHVt@tAbBb@VlBIpBHPVRr@Fh@v@dCC`AhA~@FL@j@CjBHz@C~@Iz@Sr@?NLnBm@bBIfBDjEDbBFz@Tn@Ft@W|@Kv@?x@Up@_A|@y@lAgAjC[h@_@d@s@~A_B`BmBZ_@J?FJz@`@hBJx@LbBLx@Fv@?dCIrDa@hFUrBcAfGiAxEq@|A]f@cCbC_BrA}@d@uDpAoBT_DLwF_@iAQ_D{@[g@a@OmBe@}EaBk@[}EmBcAk@a@a@oBeDq@aB_AuCW{AUoB@_AE{@?wBFu@Hu@j@}AXi@\\g@hBkBpAcBFa@s@sAaBwAmCsBQEi@d@e@Hy@@c@Uu@uA_AcAiCkBa@Ie@F_@d@c@Nc@?eAg@oAY_@`@_@h@iCnF_@^e@F_@U]e@gAcBuBsCw@qA_@c@[k@uBcDq@qAmBgDaFsG[g@_@a@yASeFiEy@iAQC]PWn@Op@gCdHqAbCgDdFMXAX}AvAa@f@Sr@GH"];
+var encodedRoutes = [];
 
 fetch(`/strava_app/activity?id=${id}`)
 .then(response => response.json())
 .then(data => {
-  console.log('Ici on a récupéré l\'activité normalement, par ex data.distance = ' + data.distance)
-  //****** var encodedRoutes = data.polylines;
-  // ici = remplacer encodedRoutes par un extract du champ d'un run, en traitant bien les backlash avant
-  //console.log('encodedRoutes = ' + data.map.summary_polyline);
-  //encodedRoutes = data.map.summary_polyline;
+  //console.log('Ici on a récupéré l\'activité normalement, par ex data.distance = ' + data.distance)
+  encodedRoutes = '["' + data.map.summary_polyline + '"]';
   // Ajout de la  map
-  var map = L.map('map').setView([55.609818, 13.003286], 13);
+  var map = L.map('map').setView([48.833, 2.333], 13);
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
