e5047d5ed93e0e7c7aa552d9811b432a7224ec87
florian <florian.carriere@gmail.com>
2022-03-08 08:00:37 +0100

    compare

--- activity.js
+++ activity.js
@@ -8,7 +8,8 @@ const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
 messageDiv.innerHTML = 'id de la requete = ' + id ;
 
-var encodedRoutes = [];
+//var encodedRoutes = [];
+var encodedRoutes_old = ["{zkrIm`inANPD?BDXGPKLATHNRBRFtAR~AFjAHl@D|ALtATj@HHJBL?`@EZ?NQ\\Y^MZURGJKR]RMXYh@QdAWf@[~@aAFGb@?j@YJKBU@m@FKZ[NSPKTCRJD?`@Wf@Wb@g@HCp@Qh@]z@SRMRE^EHJZnDHbBGPHb@NfBTxBN|DVbCBdA^lBFl@Lz@HbBDl@Lr@Bb@ApCAp@Ez@g@bEMl@g@`B_AvAq@l@    QF]Rs@Nq@CmAVKCK?_@Nw@h@UJIHOZa@xA]~@UfASn@U`@_@~@[d@Sn@s@rAs@dAGN?NVhAB\\Ox@@b@S|A?Tl@jBZpAt@vBJhATfGJn@b@fARp@H^Hx@ARGNSTIFWHe@AGBOTAP@^\\zBMpACjEWlEIrCKl@i@nAk@}@}@yBOWSg@kAgBUk@Mu@[mC?QLIEUAuAS_E?uCKyCA{BH{DDgF`AaEr@uAb@oA~@{AE}AKw@    g@qAU[_@w@[gAYm@]qAEa@FOXg@JGJ@j@o@bAy@NW?Qe@oCCc@SaBEOIIEQGaAe@kC_@{De@cE?KD[H[P]NcAJ_@DGd@Gh@UHI@Ua@}Bg@yBa@uDSo@i@UIICQUkCi@sCKe@]aAa@oBG{@G[CMOIKMQe@IIM@KB]Tg@Nw@^QL]NMPMn@@\\Lb@P~@XT"];
 
 fetch(`/strava_app/activity?id=${id}`)
 .then(response => response.json())
@@ -19,6 +20,7 @@ fetch(`/strava_app/activity?id=${id}`)
   let polyline_corrected = polyline.replace('\\','\\\\');
   encodedRoutes = '["' + polyline_corrected + '"]';
   console.log('encodedRoutes = ' + encodedRoutes);
+  console.log('encodedRoutes_old = ' + encodedRoutes_old);
   // Ajout de la  map
   // centrée sur le bois de Boulogne
   var map = L.map('map').setView([48.86427818167459, 2.245533745325779], 13);
