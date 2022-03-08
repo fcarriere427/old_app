9e545ac7f524d3e2a075fb73168940aee80ad250
florian <florian.carriere@gmail.com>
2022-03-08 22:25:58 +0100

    test

--- activity.js
+++ activity.js
@@ -25,10 +25,10 @@ fetch(`/strava_app/activity?id=${id}`)
   //console.log('polyline_corrected = ' + polyline_corrected);
 
   //polyline_obj = JSON.parse(polyline);
-  polyline_obj = '[' + polyline + ']';
+  polyline_obj = polyline.json();
   console.log('typeof encodedRoutes : ' + typeof(encodedRoutes));
   console.log('typeof polyline : ' + typeof(polyline));
-  console.log('typeof polyline=obj : ' + typeof(polyline_obj));
+  console.log('typeof polyline_obj : ' + typeof(polyline_obj));
 
   if (encodedRoutes === polyline_obj){
     console.log('encodedRoutes === polyline_obj !')
