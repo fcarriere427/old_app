fc8ac76d8bfa75b4edd15a6c1eef85ea8159b6c0
florian <florian.carriere@gmail.com>
2022-03-12 15:51:44 +0100

    update

--- activity.js
+++ activity.js
@@ -16,16 +16,22 @@ fetch(`/strava_app/activity?id=${id}`)
   // pour référence : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
   let p_id = document.createElement('p');
   p_id.innerHTML = 'id : ' + data.id;
+  mainContainer.appendChild(p_id);
   let p_name = document.createElement('p');
   p_name.innerHTML = 'name : ' + data.name;
+  mainContainer.appendChild(p_name);
   let p_moving_time = document.createElement('p');
   p_moving_time.innerHTML = 'moving_time : ' + data.moving_time;
+  mainContainer.appendChild(p_moving_time);
   let p_elevation = document.createElement('p');
   p_elevation.innerHTML = 'total_elevation_gain : ' + data.total_elevation_gain;
+  mainContainer.appendChild(p_elevation);
   let p_start = document.createElement('p');
   p_start.innerHTML = 'start_date_local : ' + data.start_date_local;
+  mainContainer.appendChild(p_start);
   let p_speed = document.createElement('p');
   p_speed.innerHTML = 'average_speed : ' + data.average_speed;
+  mainContainer.appendChild(p_speed);
 
   ////// MAP //////
   let polyline = data.map.summary_polyline;
