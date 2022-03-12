4a7a8afb3c94910ec42b14d8115f005389dc3b17
florian <florian.carriere@gmail.com>
2022-03-12 10:02:23 +0100

    test detail activités

--- activity.js
+++ activity.js
@@ -5,40 +5,47 @@ const messageDiv = document.getElementById('messageDiv');
 const queryString = window.location.search; // du type "?id=345"
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
-messageDiv.innerHTML = 'Activity:' + id ;
 
 var encodedRoute = [];
 
 fetch(`/strava_app/activity?id=${id}`)
 .then(response => response.json())
 .then(data => {
-  //console.log('Ici on a récupéré l\'activité normalement, par ex data.distance = ' + data.distance)
+  //console.log('Ici on a récupéré l\'activité, par ex data.distance = ' + data.distance)
+  ////// DETAILS DES INFOS //////
+  // pour référence : https://developers.strava.com/docs/reference/#api-models-SummaryActivity
+  const msg = document.createElement('p');
+  const txt = document.createTextNode('id : ' + data.id); msg.appendChild(txt);
+  const txt = document.createTextNode('name : ' + data.name); msg.appendChild(txt);
+  const txt = document.createTextNode('moving_time : ' + data.moving_time); msg.appendChild(txt);
+  const txt = document.createTextNode('total_elevation_gain : ' + data.total_elevation_gain); msg.appendChild(txt);
+  const txt = document.createTextNode('start_date_local : ' + data.start_date_local); msg.appendChild(txt);
+  const txt = document.createTextNode('average_speed : ' + data.average_speed); msg.appendChild(txt);
+  messageDiv.appendChild(msg);
+
+  ////// MAP //////
   let polyline = data.map.summary_polyline;
   encodedRoute = polyline.split(); // pour convertir en array...
-
-    // Ajout de la  map
-  // centrée sur le bois de Boulogne : 48.86427818167459, 2.245533745325779
-  // centrée sur Malmo : 55.609818, 13.003286
+  // On centre par défaut sur le bois de Boulogne
   var map = L.map('map').setView([48.86427818167459, 2.245533745325779], 13);
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
     }).addTo(map);
-
-  // Ajout des traces
-  for (let encoded of encodedRoute) {
+  // Ajout de la trace
+  for (let encoded of encodedRoute) { // mais en fait on ne va en récupérer qu'une !
     var coordinates = L.Polyline.fromEncoded(encoded).getLatLngs();
     L.polyline(
       coordinates,
       {
           color: 'red',
-          weight: 2,
+          weight: 3,
           opacity: .7,
           lineJoin: 'round'
       }
     ).addTo(map);
   }
-
+  // Auto-centrage et autp-zoom
   const bounds = L.latLngBounds(coordinates);
   map.fitBounds(bounds);
 
