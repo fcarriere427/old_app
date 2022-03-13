22b65a2fcf0603f066d1bb56f415ce5f5fb46ab0
florian <florian.carriere@gmail.com>
2022-03-13 01:25:15 +0100

    correct select

--- strava.js
+++ strava.js
@@ -3,7 +3,7 @@ import {strDate, strTime, strSpeed } from './functions.js';
 function main(){
   const messageDiv = document.getElementById('messageDiv');
   // bouton "List"
-  let liste_annees = [2021,2020,2019,2018,2017,2016,2015];
+  let liste_annees = [2022,2021,2020,2019,2018,2017,2016,2015];
   let select = document.createElement('select');
   select.id = 'year_filter';
   messageDiv.appendChild(select);
@@ -13,10 +13,10 @@ function main(){
     option.text = liste_annees[i];
     select.appendChild(option);
   }
-  let year = select.value;
   const listButton = document.getElementById('listButton');
   listButton.addEventListener('click', function(e) {
     cleanDiv();
+    let year = select.value;
     listActivities(year);
   });
   // bouton "update"
