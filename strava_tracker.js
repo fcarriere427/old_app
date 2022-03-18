9a249b7cc83ba954fbd18fc072df1e832d272a45
florian <florian.carriere@gmail.com>
2022-03-18 23:40:22 +0100

    test highcharts

--- strava_tracker.js
+++ strava_tracker.js
@@ -1,5 +1,6 @@
 function main(){
-  init()
+  init();
+  testGraph();
 }
 
 function init() {
@@ -8,8 +9,41 @@ function init() {
   // blocs de texte
   const titre = document.createElement('h2');
   titre.innerHTML = 'Tracker Strava';
+
+  const chart = document.createElement('div');
+  div.setAttribute('id', 'container');
+  div.setAttribute('class', 'graph');
   // création de la page
   main.appendChild(titre);
 }
+
+function testGraph(){
+  document.addEventListener('DOMContentLoaded', function () {
+        const chart = Highcharts.chart('container', {
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Fruit Consumption'
+            },
+            xAxis: {
+                categories: ['Apples', 'Bananas', 'Oranges']
+            },
+            yAxis: {
+                title: {
+                    text: 'Fruit eaten'
+                }
+            },
+            series: [{
+                name: 'Jane',
+                data: [1, 0, 4]
+            }, {
+                name: 'John',
+                data: [5, 7, 3]
+            }]
+        });
+    });
+}
+
 
 main()
